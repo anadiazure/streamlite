6686858518b6d89b1c1078f2336ee700d3840cb9
anadiazure <anadiazure@gmail.com>
2025-05-19 21:49:45 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
